7b1ba36b761439374a03ec435a569b5aee11149e
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2023-10-20 01:14:35 -0400

    incorporating the useState Hooks

--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 
 
 function App(){
-  const [status] = useState("Not Delivered");
+  const [status, setStatus] = useState("Not Delivered");
   return (
     <div>
       <h1>The package is: {status}.</h1>
+      <button onClick={()=> setStatus("Delivered")}>Deliver</button>
     </div>
   )
 }
