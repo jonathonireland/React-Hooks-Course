40393a9e999c1887b3b51035a39c22894399c927
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2023-10-21 00:18:20 -0400

    5 star rating system working

--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -7,24 +7,48 @@ const createArray = (length) => [
   ...Array(length)
 ];
 
-function Star({ selected = false }) {
-  return <FaStar color={selected ? "red": "gray"} />;
+function Star({ selected = false, onSelect }) {
+  return (
+    <FaStar 
+      color={selected ? "red": "gray"} 
+      onClick={onSelect} 
+      />
+  );
 }
 
 function StarRating({ totalStars = 5}){
-  return createArray(5).map((n,i)=> (
-    <Star key={i} />
-  ));
+  const [
+    selectedStars,
+    setSelectedStars
+  ] = useState(0);
+  return ( 
+    <>
+      {
+        createArray(5).map(
+          (n,i) => (
+            <Star 
+              key={i} 
+              selected={selectedStars > i} 
+              onSelect={()=>setSelectedStars(i+1)} 
+            />
+          )
+        )
+      }
+      <p>
+        {selectedStars} of {totalStars}
+      </p>
+    </>
+  );
 }
 
 function App(){
-  return <StarRating totalStars={10} />;
+  return <StarRating />;
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App name="Alex" />
+    <App />
   </React.StrictMode>
 );
 
