e3501af551fe3f0a4f6d561d8b37f25c6e4f0ee8
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2023-10-23 13:59:31 -0400

    Creating controlled components with useState

--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -1,35 +1,35 @@
-import React, {useReducer} from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-const initialState ={
-  message: "hi"
-}
-
-function reducer(state, action) {
-  switch(action.type){
-    case "yell":
-      return {
-        message: `HEY! I JUST SAID ${state.message}`
-      }
-    case "whisper":
-      return {
-        message: `excuse me, I just said ${state.message}`
-      }
-  }
-}
-
 function App(){
-  const [state, dispatch]= useReducer(
-    reducer,
-    initialState
-  )
+  const [sound, setSound] = useState("");
+  const [color, setColor] = useState("#000000");
+  
+
+
+  const submit = (e) => {
+      e.preventDefault();
+      alert(`${sound} sounds like ${color}`);
+      setSound("");
+      setSound("#000000");
+  }
+
   return (
-  <>
-    <p>Message: {state.message}</p>
-    <button onClick={(()=>dispatch({type: "yell"}))}>YELL</button>
-    <button onClick={()=>dispatch({type: "whisper"})}>whisper</button>
-  </>
+    <form onSubmit={submit}> 
+      <input 
+        value={sound} 
+        type="text" 
+        placeholder="Sound..." 
+        onChange={(e) => setSound(e.target.value)}
+      />
+      <input 
+        value={color} 
+        type="color" 
+        onChange={(e) => setColor(e.target.value)}
+      />
+      <button>ADD</button>
+    </form>
   );
 }
 
