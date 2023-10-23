3a013310b88eea9e3ee9688149452ea12f1c7f31
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2023-10-23 15:06:04 -0400

    creating a custom hook with context

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import React, { useContext } from "react";
 import './App.css';
-import { TreesContext } from "./";
+import { useTrees } from "./"
 
 function App(){
-  const {trees} = useContext(TreesContext);
+  const {trees} = useTrees();
   return (
     <div>
       <h1>Trees I've Heard Of</h1>
